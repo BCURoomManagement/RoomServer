7788630d89b22bd3b7fccdf605504300e9e32df5
EZJJJJ <1638623039@qq.com>
2018-10-11 10:26:13 +0800

    stime

--- src/main/java/com/example/dao/ClassTimeDao.java
+++ src/main/java/com/example/dao/ClassTimeDao.java
@@ -1,7 +1,6 @@
 package com.example.dao;
 
-import com.example.entity.ClassTime;
-import com.example.entity.Logn;
+import com.example.entity.Index;
 import com.example.util.DBUtil;
 
 import java.sql.Connection;
@@ -13,9 +12,9 @@ import java.util.List;
 
 public class ClassTimeDao {
     DBUtil util = new DBUtil();
-    public List<ClassTime> gettime(String date,int[] a) {
+    public List<Index> gettime(String date,int [] a) {
         String [] b = new String[] {"one","two","three","four","five","six","seven","eight","nine","ten","tone","ttwo","tthree","tfour"};
-        String c = "1=1";
+        String c = "";
         for(int i = 1 ; i<15 ; i++){
             if(a[i]==1){
                 c=c+" and "+b[i-1]+"=0";
@@ -24,24 +23,48 @@ public class ClassTimeDao {
         System.out.println(c);
 
         String sql2 = c;
-        String sql = "select roomid from UseRoom.usemode where "+sql2+" and data = "+date;
+        String sql = "SELECT * FROM UseRoom.classroom WHERE classroom.roomid IN (select roomid from UseRoom.usemode where data ="+date+sql2+")";
         System.out.println(sql);
         Connection conn = util.getConnection();
         try {
             PreparedStatement pstmt = conn.prepareStatement(sql);
             ResultSet rs = pstmt.executeQuery();
 
-            List<ClassTime> list = new ArrayList<ClassTime>();
+            List<Index> list2 = new ArrayList<Index>();
             while (rs.next()) {
-                ClassTime message = new ClassTime();
-                message.setRoomid(rs.getString(1));
-                list.add(message);
+                Index message2 = new Index();
+                message2.setRoomid(rs.getString(1));
+                message2.setTypee(rs.getString(2));
+                message2.setPlace(rs.getString(3));
+                message2.setPicture(rs.getString(4));
+                message2.setPeoplenum(rs.getString(5));
+                message2.setWifi(rs.getString(6));
+                message2.setProjector(rs.getString(7));
+                message2.setRoomcomputer(rs.getString(8));
+                message2.setDoorlock(rs.getString(9));
+                message2.setWired(rs.getString(10));
+                message2.setTouchtv(rs.getString(11));
+                message2.setStucomputer(rs.getString(12));
+                message2.setActdesks(rs.getString(13));
+                message2.setAio(rs.getString(14));
+                message2.setMeetingsys(rs.getString(15));
+                message2.setAircolo(rs.getString(16));
+                message2.setDesks(rs.getString(17));
+                list2.add(message2);
             }
             conn.close();
-            return list;
+            return list2;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
+    }
+    public int [] stt(String [] a) {
+        int [] result = new int [a.length];
+        for (int i = 0;i<a.length;i++){
+            result[i] = Integer.parseInt(a[i]);
+        }
+        return result;
+
     }
 }
--- src/main/java/com/example/servlet/ClassTimeServlet.java
+++ src/main/java/com/example/servlet/ClassTimeServlet.java
@@ -25,17 +25,23 @@ public class ClassTimeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-    private List<ClassTime> list = new ArrayList<ClassTime>();
+    private List<Index> list = new ArrayList<Index>();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF8");
         PrintWriter out = response.getWriter();
+        String b = new String(request.getParameter("Daydata").getBytes("iso8859-1"),"UTF-8");
+        String aa = new String(request.getParameter("time").getBytes("iso8859-1"),"UTF-8");
 
+//        int [] a=new int[]{0,0,0,1,1,0,0,0,0,0,0,0,0,0,0};
 
-        int [] a=new int[]{0,0,0,1,1,0,0,0,0,0,0,0,0,0,0};
+//        String b = "2018104";
+        String b1 = aa.substring(1,aa.length()-1);
+        String [] bb = b1.split(",");
 
-        String b = "2018104";
+        int [] a = new ClassTimeDao().stt(bb);
+
         list = new ClassTimeDao().gettime(b,a);
         System.out.println(list);
         if (list != null) {
