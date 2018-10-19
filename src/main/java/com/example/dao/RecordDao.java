fbaacf55e68c913ede60e0ffa280eede533cb32d
EZJJJJ <1638623039@qq.com>
2018-10-19 22:14:15 +0800

    submit

--- src/main/java/com/example/dao/RecordDao.java
+++ src/main/java/com/example/dao/RecordDao.java
@@ -27,10 +27,23 @@ public class RecordDao {
                 message.setUsername(rs.getString(1));
                 message.setUse(rs.getString(2));
                 message.setData(rs.getString(3));
-                message.setTime(rs.getString(4));
-                message.setMessage(rs.getString(5));
-                message.setPass(rs.getString(6));
-                message.setSubmission(rs.getString(7));
+                message.setOne(rs.getString(4));
+                message.setTwo(rs.getString(5));
+                message.setThree(rs.getString(6));
+                message.setFour(rs.getString(7));
+                message.setFive(rs.getString(8));
+                message.setSix(rs.getString(9));
+                message.setSeven(rs.getString(10));
+                message.setEight(rs.getString(11));
+                message.setNine(rs.getString(12));
+                message.setTen(rs.getString(13));
+                message.setTone(rs.getString(14));
+                message.setTtwo(rs.getString(15));
+                message.setThree(rs.getString(16));
+                message.setTfour(rs.getString(17));
+                message.setRoomid(rs.getString(18));
+                message.setPass(rs.getString(19));
+                message.setSubmission(rs.getString(20));
                 list.add(message);
             }
             conn.close();
@@ -39,5 +52,45 @@ public class RecordDao {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public boolean instertSubmit(String username,String use,String data,String a [],String roomid,String pass,String submission){
+        String sql = "insert into UseRoom.brecrord values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        Connection conn = util.getConnection();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,username);
+            pstmt.setString(2,use);
+            pstmt.setString(3,data);
+            pstmt.setString(4,a[0]);
+            pstmt.setString(5,a[1]);
+            pstmt.setString(6,a[2]);
+            pstmt.setString(7,a[3]);
+            pstmt.setString(8,a[4]);
+            pstmt.setString(9,a[5]);
+            pstmt.setString(10,a[6]);
+            pstmt.setString(11,a[7]);
+            pstmt.setString(12,a[8]);
+            pstmt.setString(13,a[9]);
+            pstmt.setString(14,a[10]);
+            pstmt.setString(15,a[11]);
+            pstmt.setString(16,a[12]);
+            pstmt.setString(17,a[13]);
+            pstmt.setString(18,roomid);
+            pstmt.setString(19,pass);
+            pstmt.setString(20,submission);
+            if (pstmt.executeUpdate()>0){
+                conn.close();
+                return true;
+            }
+            else {
+                conn.close();
+                return false;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
---
+++ src/main/java/com/example/servlet/SubmitServlet.java
@@ -0,0 +1,43 @@
+package com.example.servlet;
+
+import com.example.dao.RecordDao;
+import com.example.entity.Record;
+import net.sf.json.JSONArray;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/SubmitServlet")
+public class SubmitServlet extends HttpServlet {
+    public SubmitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=UTF8");
+        PrintWriter out = response.getWriter();
+        boolean a;
+        String [] b = new String [] {"0","0","1","1","0","0","0","0","0","0","0","0","0","0"};
+        a = new RecordDao().instertSubmit("ad","ss","20181019",b,"www","no","20181019");
+        System.out.println(a);
+        if (a != false) {
+            out.print("true");
+        } else {
+            out.print("false");
+        }
+        out.flush();
+        out.close();
+    }
+}
